911ebc377f2a250e9976764da929c250bb5a0771
bpsmanager <blueprint.dummy.1@gmail.com>
2015-12-11 08:43:55 -0500

    Chekcing in

--- src/test/java/artOfTesting/googleCalcTest.java
+++ src/test/java/artOfTesting/googleCalcTest.java
@@ -5,9 +5,4 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 public class googleCalcTest {
-} // adding some shit and more shit
-// making a change for fs trigger
-// adding more stuff 
-// added commit and add to post steps.this might be it. 
-// check if this commit updates both featuretest and master
-// corrected the syntax on commit and add in post bild
+} // adding some code, but not really
